8d808a5898666363f6907e2c21182211c806d43b
Grégory Marini <greg.marini@hotmail.fr>
2022-09-14 11:14:47 +0200

    création class timer, ajout html tag pour commencer à travailler avec le timer

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hi there");
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener("click", this.start);
+    }
+
+    start() {
+        console.log("Time to start the timer!");
+    }
+
+    pause() {}
+
+    onDurationchange() {}
+
+    tick() {}
+}
