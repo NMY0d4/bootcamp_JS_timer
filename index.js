3708798c6e63d7f45d9f3f49d29d287526c70d76
Grégory Marini <greg.marini@hotmail.fr>
2022-09-14 15:19:01 +0200

    entraînement sur différentes façon de faire le timer, utilisation d'un class Timer

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
 class Timer {
+    _interval;
+
     constructor(durationInput, startButton, pauseButton) {
         this.durationInput = durationInput;
         this.startButton = startButton;
         this.pauseButton = pauseButton;
 
         this.startButton.addEventListener("click", this.start);
+        this.pauseButton.addEventListener("click", this.pause);
     }
 
-    start() {
-        console.log("Time to start the timer!");
+    start = () => {
+        this.tick();
+        this._interval = setInterval(this.tick, 1000);
+    };
+
+    tick = () => {
+        this.timeRemaining = this.timeRemaining - 1;
+    };
+
+    pause = () => {
+        clearInterval(this._interval);
+    };
+
+    get timeRemaining() {
+        return +this.durationInput.value;
     }
 
-    pause() {}
-
-    onDurationchange() {}
-
-    tick() {}
+    set timeRemaining(time) {
+        this.durationInput.value = time;
+    }
 }
+
+const timer = new Timer(durationInput, startButton, pauseButton);
