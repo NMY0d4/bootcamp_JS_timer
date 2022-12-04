c24e33c02a36f710d832634b0ad3917db8a41b9f
Grégory Marini <greg.marini@hotmail.fr>
2022-12-04 13:33:40 +0100

    testing different things on promise

--- promise.js
+++ promise.js
@@ -1,10 +1,18 @@
-const myPromise = (NombresDeFaces) => {
-    return new Promise((res, rej) => {
+const diceResult = document.querySelector("#dice-result");
+
+const myPromise = (NombresDeFaces, duration) => {
+    const rollDice = (res, rej) =>
         setTimeout(() => {
             const rand = Math.floor(Math.random() * NombresDeFaces) + 1;
             return rand > 3
-                ? res(console.log(`gagné, résultat du dé: ${rand}`))
-                : rej(console.error(`perdu, résultat du dé: ${rand}`));
-        }, 2000);
+                ? res(`gagné, résultat du dé: ${rand}`)
+                : rej(`perdu, résultat du dé: ${rand}`);
+        }, 1000 * duration);
+    return new Promise((res, rej) => {
+        rollDice(res, rej);
     });
 };
+
+myPromise(10, 5)
+    .then((res) => (diceResult.innerHTML = res))
+    .catch((rej) => (diceResult.innerHTML = rej));
